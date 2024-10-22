8d53132bddc38e13d05093399ab1009430f49b0d
MichelangeloCaltagirone <michelangelocaltagirone@gmail.com>
2024-10-22 20:41:13 +0200

    Populated the home view with data from a temporal db ( in config ).

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -4,8 +4,36 @@
 
 @section("main-content")
 
-<h1>
-    Homepage
-</h1>
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                @foreach ($comicsList as $comic)
+                <ul>
+                    <li>
+                        Titolo: {{ $comic["title"] }}
+                    </li>
+                    <li>
+                        Tipo: {{ $comic["type"] }}
+                    </li>
+                    <li>
+                        Serie: {{ $comic["series"] }}
+                    </li>
+                    <li>
+                        Giorno di uscita: {{ $comic["sale_date"]}}
+                    </li>
+                    <li>
+                        Prezzo: {{ $comic["price"] }}
+                    </li>
+                    <li>
+                        Descrizione: {{ $comic["description"] }}
+                    </li>
+                    <li>
+                        Url immagine: {{ $comic["thumb"] }}
+                    </li>
+                </ul>
+                @endforeach
+            </div>
+        </div>
+    </div>
 
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('pages/home');
+    $comicsList = config("db.comics");
+    return view('pages/home', compact("comicsList"));
 });
