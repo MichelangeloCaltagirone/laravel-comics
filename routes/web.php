66591229e7f6ffd6fa6b21a0757327d13f6d924f
MichelangeloCaltagirone <michelangelocaltagirone@gmail.com>
2024-10-23 16:33:16 +0200

    Prepared the Route for the new page Show

--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,9 @@ Route::get('/', function () {
     $comicsList = config("db.comics");
     return view('pages/home', compact("comicsList"));
 });
+
+Route::get('/{index}', function (string $index){
+    $comics = config("db.comics");
+    $comic = $comics[$index];
+    return view('pages.show');
+});
